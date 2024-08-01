c38207a8f9ed664a871313818d2045ce3758c217
Mahshad <mahshad1368@gmail.com>
2024-08-01 14:28:03 +0200

    Sperrbildschirm fertig

--- Sperrbildschirm/ContentView.swift
+++ Sperrbildschirm/ContentView.swift
@@ -12,43 +12,59 @@ struct LockScreenButton: View {
     
     let imageSystemName : String
     var body: some View {
-        Image(systemName: imageSystemName)
-            .frame(width: 50, height: 50)
-            .background(Color.black)
-            .opacity(0.75)
-            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/, style: /*@START_MENU_TOKEN@*/FillStyle()/*@END_MENU_TOKEN@*/)
-            .foregroundColor(.white)
+        Button(action: {}, label:
+                {
+            Image(systemName: imageSystemName)
+                .frame(width: 50, height: 50)
+                .background(Color.black)
+                .opacity(0.75)
+                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/, style: /*@START_MENU_TOKEN@*/FillStyle()/*@END_MENU_TOKEN@*/)
+                .foregroundColor(.white)
+        })
+        .padding([.leading, .trailing], 50)
+        .padding(.bottom, 50)
+        
         
     }
 }
 struct ContentView: View {
     var body: some View {
         ZStack {
-            Color.gray
+            Image("mahsan")
+                .resizable()
+                .scaledToFill()
+                .frame(maxWidth: UIScreen.main.bounds.width)
+            
             VStack{
                 Image(systemName: "lock.fill")
                     .font(.largeTitle)
                     .padding(.top, 70)
                 
-                Text("06:45")
+                Text(Date(), style: .time)
                     .font(.system(size: 85, weight: .thin))
                 
-                Text("01.08.2024")
+                Text(Date(), style: .date)
                     .font(.title2)
                     .offset(y: -2)
                 Spacer()
                 HStack{
-                    LockScreenButton(imageSystemName: "flashlight.off.fill")
-                    LockScreenButton(imageSystemName: "camera.fill")
-
                     
+                    LockScreenButton(imageSystemName: "flashlight.off.fill")
+                    Spacer()
+                    LockScreenButton(imageSystemName: "camera.fill")
                 }
-                
+//                Capsule()
+//                    .fill(Color.white)
+//                    .frame(width: 180, height: 5)
+//                    .padding(.bottom, 8)
             }
-               
+            
+            
         }
+        .foregroundColor(.white)
         .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
     }
+    
 }
 
 #Preview {
